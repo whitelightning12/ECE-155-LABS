397abdfaa6f0bf43f5717e20ca9893b61b10ca94
whitelightning12 <akarner@bell.net>
2017-05-27 22:12:04 -0400

    First Commit

---
+++ app/src/main/java/lab2_202_14/lab2_202_14/Lab2_202_14.java
@@ -0,0 +1,13 @@
+package lab2_202_14.lab2_202_14;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lab2_202_14 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab2_202_14);
+    }
+}
