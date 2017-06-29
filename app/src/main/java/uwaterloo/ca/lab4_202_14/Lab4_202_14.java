bb268200799180a481e88d5882e542586eb620d4
whitelightning12 <akarner@bell.net>
2017-06-29 14:52:02 -0400

    First Commit

---
+++ app/src/main/java/uwaterloo/ca/lab4_202_14/Lab4_202_14.java
@@ -0,0 +1,13 @@
+package uwaterloo.ca.lab4_202_14;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lab4_202_14 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab4_202_14);
+    }
+}
